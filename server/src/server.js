42f3d9c68472a7ca1a0ba5a440e231e191be9d79
Arielle Farhi <ariel.farhi@stu.shenkar.ac.il>
2025-04-20 20:56:12 +0300

    Add routes to server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,38 +1,39 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import express from "express";
 import logger from "morgan";
-
 import authRoutes from "./routes/authRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
+import programRoutes from "./routes/programRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js"; 
 
 const app = express();
 const port = process.env.PORT || 8080;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 
 const corsOptions = {
-    origin: ["http://localhost:3000"],
-    credentials: true,
-}; 
+  origin: ["http://localhost:3000"],
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
-app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/profiles", profileRoutes);
+app.use("/api/programs", programRoutes);
+app.use("/api/reviews", reviewRoutes);
 
 app.use((req, res) => {
-    res.status(404).send("Page not found");
+  res.status(404).send("Page not found");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
