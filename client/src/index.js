839bff60d8d6e481bee251dc5c20be9df395d7cf
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-04-22 01:28:39 +0300

    fix index.js
    change jsx to js

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+//need to change
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import { SearchProvider } from "./context/Search";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <QueryClientProvider client={queryClient}>
+      <SearchProvider>
+        <App />
+        <Toaster />
+      </SearchProvider>
+    </QueryClientProvider>
+  </>
+);
