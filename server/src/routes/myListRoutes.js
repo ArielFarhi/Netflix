db9967f966833d1cc24e250059bc389b7438e0cc
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-04-22 01:06:01 +0300

    Create myListRoute.js & add protected routes for adding, fetching, and removing movies from user's list

---
+++ server/src/routes/myListRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { addMovie, getMyList, removeMovie } from "../controllers/myListController.js";
+import protect from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.use(protect);
+
+router.get("/", getMyList);
+
+router.post("/", addMovie);
+
+router.delete("/:movieId", removeMovie);
+
+export default router;
