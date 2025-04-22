b0789d220d5f73d8fc74ee48ca817a9cf448dada
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-04-22 16:17:23 +0300

    Update program.js

--- server/src/models/Program.js
+++ server/src/models/Program.js
@@ -1,3 +1,17 @@
+// import mongoose from "mongoose";
+
+// const programSchema = new mongoose.Schema({
+//   title: { type: String, required: true },
+//   genre: { type: String, required: true },
+//   description: { type: String },
+//   rating: { type: Number, default: 0 },
+//   releaseDate: { type: Date },
+//   type: { type: String, enum: ["movie", "tv-show"] },
+//   episodes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Episode" }],
+// });
+
+// export default mongoose.model("Program", programSchema);
+
 import mongoose from "mongoose";
 
 const programSchema = new mongoose.Schema({
@@ -8,6 +22,11 @@ const programSchema = new mongoose.Schema({
   releaseDate: { type: Date },
   type: { type: String, enum: ["movie", "tv-show"] },
   episodes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Episode" }],
+
+  // ✅ תוספות חדשות:
+  originalLanguage: { type: String }, // שפת מקור
+  subtitleLanguages: [{ type: String }], // כתוביות
+  dubbingLanguages: [{ type: String }], // דיבוב
 });
 
 export default mongoose.model("Program", programSchema);
