c2124a5747756ffe77b588986e7b152a6dacb816
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-04-25 02:44:34 +0300

    edit server

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,54 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import express from "express";
+// import logger from "morgan";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// import authRoutes from "./routes/authRoutes.js";
+// import profileRoutes from "./routes/profileRoutes.js";
+// import programRoutes from "./routes/programRoutes.js";
+// import reviewRoutes from "./routes/reviewRoutes.js";
+// import movieListRoutes from "./routes/movieListRoutes.js";
+
+// const app = express();
+// app.set("trust proxy", 1);
+// const port = process.env.PORT || 8080;
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// app.use("/", express.static(path.join(__dirname, "../public")));
+
+// const corsOptions = {
+//   origin: ["http://localhost:3000", "https://netflix-zguc.onrender.com"],
+//   credentials: true,
+// };
+
+// app.use(cors(corsOptions));
+// app.use(cookieParser());
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(logger("dev"));
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/profiles", profileRoutes);
+// app.use("/api/programs", programRoutes);
+// app.use("/api/reviews", reviewRoutes);
+// app.use('/api/movie-list', movieListRoutes);
+
+// app.use((req, res) => {
+//   res.status(404).send("Page not found");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -18,19 +69,21 @@ const app = express();
 app.set("trust proxy", 1);
 const port = process.env.PORT || 8080;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
-app.use("/", express.static(path.join(__dirname, "../public")));
+app.use("/", express.static(path.join(dirname, "../public")));
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://netflix-zguc.onrender.com"],
+  origin: [
+    "http://localhost:3000",
+    "https://aesthetic-caramel-c5c860.netlify.app"
+  ],
   credentials: true,
 };
-
 app.use(cors(corsOptions));
-app.use(cookieParser());
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
@@ -39,7 +92,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/profiles", profileRoutes);
 app.use("/api/programs", programRoutes);
 app.use("/api/reviews", reviewRoutes);
-app.use('/api/movie-list', movieListRoutes);
+app.use("/api/movie-list", movieListRoutes);
 
 app.use((req, res) => {
   res.status(404).send("Page not found");
