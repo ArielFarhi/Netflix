cb9907d3ec983f6e33899cfcd8b59939153026be
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-04-24 16:58:46 +0300

    minor formatting fix in addMovie controller

--- server/src/controllers/movieListController.js
+++ server/src/controllers/movieListController.js
@@ -1,14 +1,14 @@
 import movieListService from "../services/movieListService.js";
 
 export const addMovie = async (req, res) => {
-  try {
-    console.log("📥 req.body:", req.body);
-    const movie = await movieListService.addMovieToList(req.body);
-    res.status(201).json(movie);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
+    try {
+      console.log("📥 req.body:", req.body); 
+      const movie = await movieListService.addMovieToList(req.body);
+      res.status(201).json(movie);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
 
 export const getMyList = async (req, res) => {
   try {
