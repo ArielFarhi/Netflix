36baf9e9ef7ec8b6b2a558b0c6bd6a62715f44cf
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-04-25 01:42:58 +0300

    Update

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL || "https://netflix-szyh.onrender.com/api";
+const BASE_URL ="https://netflix-zguc.onrender.com";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
