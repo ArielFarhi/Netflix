0824ad76fc64ca4aa5a174f8975e9f7124610383
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-04-24 16:59:11 +0300

    Delete unnecessary spaces

--- server/src/models/MovieList.js
+++ server/src/models/MovieList.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 const movieListSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user' }, 
-  movieId: { type: Number, required: true }, 
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+  movieId: { type: Number, required: true },
   title: { type: String, required: true },
   posterPath: { type: String },
 });
