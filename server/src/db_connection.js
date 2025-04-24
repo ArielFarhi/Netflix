fad5f534d33c063a0f3f8ea3cf7d6dac8c7e33eb
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-04-24 17:33:04 +0300

    delete console.log

--- server/src/db_connection.js
+++ server/src/db_connection.js
@@ -6,10 +6,6 @@ import mongoose from "mongoose";
 const { DB_HOST, DB_USER, DB_PASSWORD } = process.env;
 const url = DB_HOST;
 
-console.log("DB_HOST:", DB_HOST);
-console.log("DB_USER:", DB_USER);
-console.log("DB_PASSWORD:", DB_PASSWORD);
-
 const options = {
   user: DB_USER,
   pass: DB_PASSWORD,
